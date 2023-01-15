6b50043eb7efca7a5cf9fb5dc8f4653a5db99d5c
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-01-15 21:48:14 +0800

    finish hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,11 +1,30 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst)
+   (if (null? asc-lst)
+        #t
+        (if (null? (cdr asc-lst))
+            #t
+            (if (> (car asc-lst) (cadr asc-lst))
+                #f
+                (ascending? (cdr asc-lst))
+            )
+        )
+   )
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+    (if (= exp 0)
+        1
+        (if (even? exp)
+            (square (pow base (/ exp 2)))
+            (* base (pow base (- exp 1)))
+        )
+    )
+)
