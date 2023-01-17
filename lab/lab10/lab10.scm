c511cdd14ffbc3f54df9cc7fdc4433b5a9332b9b
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-01-17 22:05:44 +0800

    finish lab10

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,25 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (cond ((< num1 num2) -1)
+          ((= num1 num2) 0)
+          (else          1)
+    )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+    (lambda (inc) (+ num inc))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+    (lambda (x) (f (g x)))
+)
 
-(define lst 'YOUR-CODE-HERE)
+(define lst
+    '((1) 2 (3 4) 5)
+)
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst)
+    (if (null? lst)
+        nil
+        (cons (car lst) (cons (car lst) (duplicate (cdr lst))))
+    )
+)
