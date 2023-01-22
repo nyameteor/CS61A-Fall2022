c8b126b3077ba455a7875424d7b2ba9e56b0bbab
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-01-22 21:24:19 +0800

    finish proj scheme part 4

--- proj/scheme/questions.scm
+++ proj/scheme/questions.scm
@@ -7,7 +7,14 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (define (helper s n)
+    (if (null? s)
+      nil
+      (cons (list n (car s) ) 
+        (helper (cdr s) (+ n 1)))
+      )
+    )
+  (helper s 0)
   )
   ; END PROBLEM 15
 
@@ -17,7 +24,14 @@
 ;; the merged lists.
 (define (merge ordered? list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond 
+    ((null? list1) list2)
+    ((null? list2) list1)
+    ((ordered? (car list1) (car list2)) 
+      (cons (car list1) (merge ordered? (cdr list1) list2)))
+    (else 
+      (cons (car list2) (merge ordered? list1 (cdr list2))))
+    )
   )
   ; END PROBLEM 16
 
