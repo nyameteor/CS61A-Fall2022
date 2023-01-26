c0118febb4b042ae26424b9d046634af09a27e4b
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-01-26 15:06:59 +0800

    finish lab11

--- lab/lab11/buffer.py
+++ lab/lab11/buffer.py
@@ -53,20 +53,22 @@ class Buffer:
         """Remove the next item from self and return it. If self has
         exhausted its source, returns None."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        token, self.index = self.current(), self.index + 1
+        return token
         # END PROBLEM 1
 
     def current(self):
         """Return the current element, or None if none exists."""
-        while _________:
+        while not self.more_on_line():
             try:
                 # BEGIN PROBLEM 1
-                "*** YOUR CODE HERE ***"
+                self.current_line, self.index = next(self.source), 0
+                return self.current_line[self.index]
                 # END PROBLEM 1
             except StopIteration:
                 self.current_line = ()
                 return None
-        return __________
+        return self.current_line[self.index]
 
     def more_on_line(self):
         return self.index < len(self.current_line)
