a3a5fc5a7bcde55449b41c8b50abe5bd9c741fa0
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-02-11 15:24:26 +0800

    try scheme_contest

--- proj/scheme_contest/contest.scm
+++ proj/scheme_contest/contest.scm
@@ -2,16 +2,43 @@
 ;;;
 ;;; Please do not include your name or personal info in this file.
 ;;;
-;;; Title: <Your title here>
+;;; Title: Koch snowflake
 ;;;
 ;;; Description:
-;;;   <It's your masterpiece.
-;;;    Use these three lines to describe
-;;;    its inner meaning.>
+;;;    A simple implementation of Koch snowflake.
+;;;    May contain redundant recursions that need to be removed.
+;;;    References: https://en.wikipedia.org/wiki/Koch_snowflake
 
 (define (draw)
-  ; YOUR CODE HERE
+  (begin (penup)
+         (goto -364 -210)
+         (right 30)
+         (color "blue")
+         (pendown))
+  (koch-snowflake 729 5)
   (exitonclick))
+
+;; Draw Koch snowflake
+(define (koch-snowflake x n)
+  (define (edge-loop m)
+    (if (= m 0)
+        nil
+        (begin (koch-edge x n)
+               (right 120)
+               (edge-loop (- m 1)))))
+  (edge-loop 3))
+
+;; Draw one edge of Koch snowflake
+(define (koch-edge x n)
+  (if (= n 0)
+      (fd x)
+      (begin (koch-edge (/ x 3) (- n 1))
+             (left 60)
+             (koch-edge (/ x 3) (- n 1))
+             (right 120)
+             (koch-edge (/ x 3) (- n 1))
+             (left 60)
+             (koch-edge (/ x 3) (- n 1)))))
 
 ; Please leave this last line alone.  You may add additional procedures above
 ; this line.
